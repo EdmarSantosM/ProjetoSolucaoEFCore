c69fd1d7cadb8e4da053c8a290981449aeab1915
EdmarSantosM <santosem4@hotmail.com>
2021-09-30 00:34:15 -0300

    Update ApplicationContext.cs
    ..

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,13 +1,21 @@
 using System.Data;
 using Microsoft.EntityFrameworkCore;
+using CursoEFCore.Domain;
 
 namespace CursoEFCore.Data
 {
+    public DbSet<Pedido> Pedidos{get; set;}
+
     public  class ApplicationContext : DbContext
     {
         protected override void OnConfigure(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqldb;Initial Catalog=CursoEFCore;Integrated Security=true");
         }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+       {
+           
+       }
     }
 }
